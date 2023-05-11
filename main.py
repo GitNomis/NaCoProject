126082f8963ba5411e5f20d00343035e84f215f9
Racemuis <chiara.thoeni@ru.nl>
2023-05-11 18:03:30 +0200

    [feat] add initial rules

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from src import Environment, Display
+from src.Swarm import Swarm
 
 def main():
     env = Environment.Environment(size=(5, 5), swarm_size=10, fire_size=0.5)
     print(env.grid)
     print(Display.display(env))
+    for iter in range(10):
+        env.update()
+
 
 if __name__ == '__main__':
     main()
