b5953676aa9d1578419ca6e3ab1d030937e19e9c
Racemuis <chiara.thoeni@ru.nl>
2023-05-13 18:08:33 +0200

    [feat]: add water

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     show_display=True
     steps=50
 
-    env = Environment.Environment(size=(5, 5), swarm_size=10, fire_size=0.5)
+    env = Environment.Environment(size=(5, 5), swarm_size=10, fire_size=0.5, water_size=4)
     display = Display.Display(env, steps=steps)
 
     if show_display:
