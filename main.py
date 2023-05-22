7199bcb9836ae02e6977e23e849bf4ac26a1b83c
Suzan Lejeune <suzan.lejeune@gmail.com>
2023-05-22 02:06:45 +0200

    [fix] marker plotting direction

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import itertools
 def main():
     show_display=True
     steps=100
+    infinite=True
 
-    env = Environment(size=(5, 5), swarm_size=1, fire_size=1, water_size=1)
-    display = Display(env, steps=steps)
+    env = Environment(size=(5, 5), swarm_size=5, fire_size=1, water_size=1)
+    display = Display(env, steps=steps, infinite=infinite)
 
     if show_display:
         display.display()
--- src/display.py
+++ src/display.py
@@ -14,15 +14,17 @@ from .state import State
 
 class Display:
 
-    def __init__(self, env:Environment, steps:int) -> None:
+    def __init__(self, env:Environment, steps:int, infinite:bool) -> None:
         """Display class with different ways of displaying an environment.
 
         Args:
             env (Environment): environment to display
-            steps (int): number of steps
+            steps (int): number of simulation steps
+            infinite (bool): whether the simulation runs infinitely or not
         """
         self.env = env
         self.steps = steps
+        self.infinite = infinite
 
         self.cmap = colors.ListedColormap(['moccasin','firebrick','deepskyblue','yellow'])
         bounds=[-0.5, 0.5, 1.5, 2.5, 3.5]
@@ -32,46 +34,51 @@ class Display:
 
     def display(self)-> None:
         fig, self.ax = plt.subplots(1, 1, figsize=(self.env.grid.shape[0], self.env.grid.shape[1]))
-        self.ax.set_xlim([0,self.env.grid.shape[0]])
-        self.ax.set_ylim([0,self.env.grid.shape[1]])
 
-        # Forest fire heatmap
-        sns.heatmap(self.env.grid.T, cbar=False, cmap=self.cmap, norm=self.norm)
+        # Forest fire
+        plt.imshow(np.flip(self.env.grid.T, 0), cmap=self.cmap, norm=self.norm, origin="lower", extent=(0, self.env.grid.shape[0], self.env.grid.shape[1], 0))
         
         # Boids scatterplot
         x = [boid.position[0] for boid in self.env.swarm.boids]
         y = [boid.position[1] for boid in self.env.swarm.boids]
         self.scatter = plt.scatter(x, y, marker=self.boid_marker, color="grey")
 
-        # Animation
-        animator = animation.FuncAnimation(fig, self.animate, interval=self.interval, frames=self.steps, repeat=False, cache_frame_data=False)
-
         self.ax.invert_yaxis()
         plt.tight_layout()
-        plt.show(block=False)
-        plt.pause(self.steps * self.interval * 0.001) # pause (s) = frames * interval (ms) * 0.001
-        plt.close(fig)
+        
+        if not self.infinite:
+            # Animation
+            animator = animation.FuncAnimation(fig, self.animate, interval=self.interval, frames=self.steps, repeat=False, cache_frame_data=False)
+            plt.show(block=False)
+            plt.pause(self.steps * self.interval * 0.001) # pause (s) = frames * interval (ms) * 0.001
+            plt.close(fig)
+        else:
+            # Animation
+            animator = animation.FuncAnimation(fig, self.animate, interval=self.interval, cache_frame_data=False)
+            plt.show()
         
     def animate(self, i:int)-> None:
-        # Clear axes
-        self.ax.cla()
+        # Update the boids
+        self.env.update()
         
-        # Redraw forest fire heatmap
-        sns.heatmap(ax=self.ax, data=self.env.grid.T, cbar=False, cmap=self.cmap, norm=self.norm)
+        # Clear axes & set limits
+        self.ax.cla()
+        self.ax.set_xlim([0,self.env.grid.shape[0]])
+        self.ax.set_ylim([0,self.env.grid.shape[1]])
+        
+        # Redraw forest fire
+        plt.imshow(np.flip(self.env.grid.T, 0), cmap=self.cmap, norm=self.norm, origin="lower", extent=(0, self.env.grid.shape[0], self.env.grid.shape[1], 0))
         
         # Redraw boids scatterplot
         x = [boid.position[0] for boid in self.env.swarm.boids]
         y = [boid.position[1] for boid in self.env.swarm.boids]
         self.scatter = plt.scatter(x, y, marker=self.boid_marker, color="grey")
-        
-        # Update the boids
-        self.env.update()
 
-        # TODO: Add updating direction for plotting
         offsets = []
         markers = []
         colors = []
         
+        # Boid transformations (direction, scaling, color)
         for boid in self.env.swarm.boids:
             offsets.append([boid.position[0], boid.position[1]])
             marker = MarkerStyle(">")
@@ -84,7 +91,6 @@ class Display:
             markers.append(marker)
             
         self.scatter.set_color(colors)
-        
         self.scatter.set_offsets(offsets)
         paths = [m.get_path().transformed(m.get_transform()) for m in markers]
         self.scatter.set_paths(paths)
